5e1a33acfa7e26fa5452e308ce3429290fae79dc
Aznatdor <andbob-ipt25@lll.kpi.ua>
2024-03-31 11:00:47 +0300

    Add docstring to the helper functions

--- Lab 4/helper_functions.py
+++ Lab 4/helper_functions.py
@@ -1,6 +1,17 @@
 import numpy as np
 
 def create_circle(n, radius=1):
+    """
+    Creates an array with n points evenly spaced on a circle
+
+    Args:
+        n: int
+        radius: float
+
+    Returns:
+        circle: np.array
+        perimeter: float
+    """
     circle = []
     theta = 0
     delta = 2 * np.pi / n
@@ -17,6 +28,15 @@ def create_circle(n, radius=1):
     return np.array(circle), n * side_length
 
 def read_tsp_file(filepath):
+    """
+    Reads a TSP file
+
+    Args:
+        filepath: str
+
+    Returns:
+        tsp: np.array
+    """
     tsp = []
     with open(filepath) as file:
 
